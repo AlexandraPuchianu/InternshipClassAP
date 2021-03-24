f67932aadf84f51fbffc418ab898c55b69e56a78
AzureAD\Alexandra-GeorgianaP <alexandra.puchianu@principal33.com>
2021-03-24 13:30:51 +0200

    addNewcomer functionality

---
+++ wwwroot/js/default.js
@@ -0,0 +1,13 @@
+﻿let addNewComerBtn = document.getElementById("add-btn");
+addNewComerBtn.addEventListener('click', addNewcomer);
+
+function addNewcomer() {
+    var newcomer = document.getElementById("newcomer");
+    if (newcomer.value != '') {
+        var node = document.createElement("li");
+        node.innerText = newcomer.value;
+        list.appendChild(node);
+        newcomer.value = '';
+    }
+
+}
