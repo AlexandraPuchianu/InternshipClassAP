3c3b50b2e262a717554997b22d892e82a51247b8
AzureAD\Alexandra-GeorgianaP <alexandra.puchianu@principal33.com>
2021-03-26 11:59:03 +0200

    Create modal windows. Use closest

--- wwwroot/js/Index.js
+++ wwwroot/js/Index.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
                 // Remember string interpolation
                 $("#list").append(`
                     <li class="member">
-                        <span class="name">${data}</span><span class="fa fa-pencil"></span><i class="delete fa fa-remove"></i>
+                        <span class="name">${data}</span><span class="fa fa-pencil"></span><i class="startEdit fa fa-pencil" data-toggle="modal" data-target="#editClassmate"></i>
                     </li>`);
 
                 $("#newcomer").val("");
@@ -28,7 +28,7 @@ $(document).ready(function () {
     })
 
     $("#list").on("click", ".delete", function () {
-        var $li = $(this).parent('li');
+        var $li = $(this).closest('li');
         var index = $li.index();
 
         console.log(`index=${index}`);
@@ -38,15 +38,28 @@ $(document).ready(function () {
             method: "DELETE",
             url: `/Home/RemoveMember?index=${index}`,
             success: function () {
-
-
-
                 $li.remove();
             },
             error: function (data) {
                 alert(`Failed to remove`);
             },
         });
+    })
+
+    $("#list").on("click", ".startEdit", function () {
+        var targetMemberTag = $(this).closest('li');
+        var index = targetMemberTag.index(targetMemberTag.parent());
+        var currentName = targetMemberTag.find(".name").text();
+        $('#editClassmate').attr("memberIndex", index);
+        $('#classmateName').val(currentName);
+    })
+
+    $("#editClassmate").on("click", "#submit", function () {
+        console.log('submit changes to server');
+    })
+
+    $("#editClassmate").on("click", "#cancel", function () {
+        console.log('cancel changes');
     })
     
 })
