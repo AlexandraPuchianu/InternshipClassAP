1b7857b5cfe8ba4512454daddf7acdac157740d0
AzureAD\Alexandra-GeorgianaP <alexandra.puchianu@principal33.com>
2021-04-01 09:44:00 +0300

    refactor TemperatureC to lambda funct

--- Models/WeatherForecast.cs
+++ Models/WeatherForecast.cs
@@ -7,13 +7,7 @@ namespace InternshipClass.WebAPI
     {
         public DateTime Date { get; set; }
 
-        public int TemperatureC
-        {
-            get
-            {
-                return (int)(TemperatureK - 273.15);
-            }
-        }
+        public int TemperatureC => (int)(TemperatureK - 273.15);
 
         public string Summary { get; set; }
 
