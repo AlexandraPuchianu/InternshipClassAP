872441ebd28e0158e91dfbbc174aee6b272b55d5
AzureAD\Alexandra-GeorgianaP <alexandra.puchianu@principal33.com>
2021-04-14 11:58:45 +0300

    fixed add member id

--- wwwroot/js/subscribeOnChanges.js
+++ wwwroot/js/subscribeOnChanges.js
@@ -5,7 +5,7 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/messagehub").build
 
 connection.on("AddMember", function (name, id) {
     // Remember string interpolation
-    $("#list").append(`<li class="member">
+    $("#list").append(`<li class="member" memberId="${id}">
 	   <span class="name">${name}</span><span class="delete fa fa-remove"></span><i class="startEdit fa fa-pencil" data-toggle="modal" data-target="#editClassmate"></i>
 	</li>`);
 });
