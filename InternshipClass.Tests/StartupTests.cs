9ddd4dd6b4468632cb13d4544020d2b9907ac235
AzureAD\Alexandra-GeorgianaP <alexandra.puchianu@principal33.com>
2021-04-15 14:06:51 +0300

    Added test for corrupt url exception

--- InternshipClass.Tests/StartupTests.cs
+++ InternshipClass.Tests/StartupTests.cs
@@ -21,5 +21,17 @@ namespace InternshipClass.Tests
             //Assert
             Assert.Equal("Server=ec2-99-80-200-225.eu-west-1.compute.amazonaws.com;Port=5432;Database=d9bnpori3022u7;User Id=cueheijfcjxkbc;Password=c2417b22d39b9642df0d8ac9c6921fcfce37968b2248ee6caefee8621745b247;Pooling=true;SSL Mode=Require;Trust Server Certificate=True;", herokuConnectionString);
         }
+        
+        [Fact]
+        public void ShouldThrowExceptionOnCorruptUrl()
+        {
+            //Assume
+            string url = "Server=127.0.0.1;Port=5432;Database=InternshipClass;User Id=admin-internshipclass;Password=NFyzcTY3;";
+
+            //Act & Assert
+            var exception = Assert.Throws<FormatException>(() => Startup.ConvertDatabaseUrlToHerokuString(url));
+
+            Assert.StartsWith("Database Url cannot be converted!", exception.Message);
+        }
     }
 }
