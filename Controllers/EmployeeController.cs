1e0c7535d45d0991807652abc7d05391c372b2a3
AzureAD\Alexandra-GeorgianaP <alexandra.puchianu@principal33.com>
2021-04-27 22:28:00 +0300

    Created EmployeeController.
    Added API methods for get all employees, get employees by id and add new employee.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InternshipClass.Models;
+using InternshipClass.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InternshipClass.Controllers
+{
+    [Route("employee/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeDbService employeeDbService;
+
+        public EmployeeController(EmployeeDbService employeeDbService)
+        {
+            this.employeeDbService = employeeDbService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            return employeeDbService.GetEmployees();
+        }
+
+        [HttpGet("{id}")]
+        public Employee Get(int id)
+        {
+            return employeeDbService.GetEmployeeById(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Employee employee)
+        {
+            employeeDbService.AddEmployee(employee);
+        }
+    }
+}
